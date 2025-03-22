2c48feef5b19263b0b7d4d8338616bfc60b5c1dc
MiketheEmily45 <miguel@psyber.com.br>
2025-03-22 03:11:34 -0300

    adicionando acesso à biblioteca de projetos ao frontend

--- frontend/src/app/(paginas)/page.tsx
+++ frontend/src/app/(paginas)/page.tsx
@@ -1,15 +1,22 @@
 import Curriculo from "@/components/curriculo";
 import Principal from "@/components/landing/principal"
+import Projetos from "@/components/projetos/Projetos";
 import Cabecalho from "@/components/shared/Cabecalho"
 import Container from "@/components/shared/Container";
+import { obterProjetos } from "@/functions/projetos";
 import { obterTecnologias } from "@/functions/tecnologias";
 
 export default async function Home() {
   const tecnologias = await obterTecnologias()
+  const projetos = await obterProjetos()
   return (
     <div>
-      <Principal />
-      <Container className="py-16">
+      <Principal tecnologias={tecnologias.destaques}/>
+      <Container className="py-16 flex flex-col gap-7">
+        <Projetos titulo="Destaque" lista={projetos.destaque} />
+        <Projetos titulo="Web" lista={projetos.web} />
+        <Projetos titulo="Mobile" lista={projetos.mobile} />
+        <Projetos titulo="Jogos" lista={projetos.jogo} />
         <Curriculo tecnologias={tecnologias.todas}/>
       </Container>
     </div>
