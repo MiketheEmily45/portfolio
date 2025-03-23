74af33f9c7b2cb1445c9aa58ba48c862fd7cbab1
MiketheEmily45 <miguel@psyber.com.br>
2025-03-23 18:33:35 -0300

    erros corrigidos, chat funcional

---
+++ frontend/src/hooks/useChat.ts
@@ -0,0 +1,55 @@
+import { Id } from "@core";
+import useLocalStorage from "./useLocalStorage";
+import Mensagem from "@/model/Mensagem";
+import conversar from "@/functions/chat";
+import { useState } from "react";
+
+export default function useChat() {
+    const [chatId] = useLocalStorage<string>("chatId", Id.gerar())
+    const [mensagens, setMensagens] = useLocalStorage<Mensagem[]>("mensagens", [])
+    const [pensando, setPensado] = useState(false)
+
+    async function adicionarMensagem(texto: string) {
+
+        try{
+            setPensado(true)
+            const novaMensagem: Mensagem = {
+                id: Id.gerar(),
+                texto,
+                autor: "Visitante",
+                lado: "direito",
+                icone: null
+            }
+    
+            setMensagens((msgs) => [...msgs, novaMensagem])
+    
+            const resposta = await conversar(chatId, novaMensagem)
+
+            if(!resposta) return
+    
+            const mensagemResposta: Mensagem = {
+                id: Id.gerar(),
+                texto: resposta,
+                autor: "Assistente",
+                lado: "esquerdo",
+                icone: null
+            }
+
+            setMensagens((msgs) => [...msgs, mensagemResposta])
+        } finally{
+            setPensado(false)
+        }
+    }
+
+    function limparMensagens() {
+        setMensagens([])
+    }
+
+    return {
+        chatId,
+        mensagens,
+        pensando,
+        adicionarMensagem,
+        limparMensagens,
+    }
+}
